1225e6c951ab45a7e5427a87d4966810adc06648
Harsh Rana <harshrana8454@gmail.com>
2024-09-26 13:56:35 +0530

    project initial working

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import connectDB from './db/index.db.js'
+import dotenv from 'dotenv';
+import { app }from './app.js'
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+.then (()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Listening on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("Error in port: ", error);
+    
+})
