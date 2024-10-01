86daa5fdcbe2b665ac39bf78cee9c65e8d228dbb
Harsh Rana <harshrana8454@gmail.com>
2024-10-01 12:03:09 +0530

    header and footer added, with inital website design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Footer from '../src/components/Footer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../src/components/Home'
+import Header from './components/Header';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Router>
+    <div className="min-h-screen flex flex-col">
+      <Header/>
+      <div className='flex-grow'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
+      </div>
+      <Footer/>
+      </div>
+    </Router>
+    </>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-text': '#1C2833',
+        'golden': '#D4AF37',
+        'light-golden': '#F2D16D'
+      }
+    },
   },
   plugins: [],
 }
